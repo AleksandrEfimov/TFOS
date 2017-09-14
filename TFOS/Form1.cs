26f3edb4c166b0261c15645d78053c9d81aa433a
AleksandrEfimov <aleks.efy@gmail.com>
2017-09-14 19:24:42 +0300

    2 button: 1 перебор меню, 2 проверка наличия стикеров
    2 button: 1 перебор меню, 2 проверка наличия стикеров

--- TFOS/Form1.cs
+++ TFOS/Form1.cs
@@ -7,6 +7,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
 
 namespace TFOS
 {
@@ -15,6 +19,186 @@ namespace TFOS
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            IWebDriver driver = new ChromeDriver();
+            WebDriverWait wait;
+            string log = "";
+            // set timeout
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+
+
+            // заходим на страничку
+            driver.Url = "http://localhost:8080/litecart/admin/";
+            // логин
+            IWebElement element = wait.Until(d => d.FindElement(By.Name("username")));
+            driver.FindElement(By.Name("username")).SendKeys("admin");
+            // пароль
+            //// в поисках ошибки применён xPath - не помог, но работает.
+            driver.FindElement(By.XPath("//*[@id=\"box-login\"]/form/div[1]/table/tbody/tr[2]/td[2]/span/input")).SendKeys("admin");
+            //// данному логину необходим был Клик, в то время как Гугл требовал сабмит
+            driver.FindElement(By.Name("login")).Click();
+
+
+            // как критерий входа в админ-панель - виджет статистики интернет-магазина
+            try
+            {
+                var login = wait.Until(ExpectedConditions.ElementExists(By.Id("widget-stats")));
+                //if (login != null)
+                  //  MessageBox.Show("Login completed");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Check the site");
+            }
+
+            //int countmenu = driver.FindElements(By.CssSelector("#app- > a")).Count;
+            //MessageBox.Show("Amount menu item: "+countmenu);
+            //IWebElement menu = driver.FindElements(By.CssSelector("#app- > a"));
+
+            var menuItem = driver.FindElements(By.CssSelector("#app-"));
+            int countMenuItem = menuItem.Count;
+
+
+            //for (int i = 0; i < countMenuItem; i++)
+            //{                
+            //    // refresh
+            //    menuItem = driver.FindElements(By.CssSelector("#app-"));
+
+            //    // проверим есть ли открытые подменю
+            //    var menuSubItem = driver.FindElements(By.CssSelector("ul.docs li"));
+            //    int countMenuSubItem = menuSubItem.Count;
+
+            //    if (countMenuSubItem > 0 )
+            //    {
+            //            for (int j = 0; j < countMenuSubItem; j++)
+            //            {
+            //                // refresh
+            //                menuSubItem = driver.FindElements(By.CssSelector("ul.docs li"));
+            //                menuSubItem[j].Click();
+            //                log += "Меню: " + i + " подменю: " + j+",\n";
+            //            }
+
+            //            continue;
+            //    }
+
+            //    menuItem[i].Click();
+            //    log += "Меню: " + i + ",\n";
+            //}
+            int i = -1;
+            do
+            {
+                
+                //refresh
+                menuItem = driver.FindElements(By.CssSelector("#app-"));
+
+                    // проверим есть ли открытые подменю
+                    var menuSubItem = driver.FindElements(By.CssSelector("ul.docs li"));
+                    int countMenuSubItem = menuSubItem.Count;
+
+                    if (countMenuSubItem > 0)
+                    {
+                        for (int j = 0; j < countMenuSubItem; j++)
+                        {
+                            // refresh
+                            menuSubItem = driver.FindElements(By.CssSelector("ul.docs li"));
+                            menuSubItem[j].Click();
+                            log += "Меню: " + i + " подменю: " + j + ",\n";
+                        }
+
+                        // готовим переход к следующему пункту меню
+                        i++;
+                    //refresh
+                        if (i < countMenuItem)
+                        {
+                            menuItem = driver.FindElements(By.CssSelector("#app-"));
+                            menuItem[i].Click();
+                            continue;
+                        }
+                    }
+
+                menuItem[++i].Click();
+               
+                log += "Меню: " + i + ",\n";
+
+            } while (i < countMenuItem);
+
+            MessageBox.Show(log);
+
+            //var menuSubItem = driver.FindElements(By.CssSelector("ul.docs li"));
+            //if (menuSubItem != null)
+            //    MessageBox.Show("Menu Sub Item Count:" + menuSubItem.Count);
+
+
+
+            driver.Quit();
+            driver = null;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            IWebDriver driver = new ChromeDriver();
+            WebDriverWait wait;
+            string log = "";
+            // set timeout
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+
+            driver.Url = "http://localhost:8080/litecart/";
+            // заходим на страничку
+            //driver.Url = "http://localhost:8080/litecart/admin/";
+            //// логин
+            //IWebElement element = wait.Until(d => d.FindElement(By.Name("username")));
+            //driver.FindElement(By.Name("username")).SendKeys("admin");
+            //// пароль
+            ////// в поисках ошибки применён xPath - не помог, но работает.
+            //driver.FindElement(By.XPath("//*[@id=\"box-login\"]/form/div[1]/table/tbody/tr[2]/td[2]/span/input")).SendKeys("admin");
+            ////// данному логину необходим был Клик, в то время как Гугл требовал сабмит
+            //driver.FindElement(By.Name("login")).Click();
+
+
+            //// как критерий входа в админ-панель - виджет статистики интернет-магазина
+            //try
+            //{
+            //    var login = wait.Until(ExpectedConditions.ElementExists(By.Id("widget-stats")));
+            //    //if (login != null)
+            //    //  MessageBox.Show("Login completed");
+            //}
+            //catch (Exception ex)
+            //{
+            //    MessageBox.Show("Check the site");
+            //}
+
+
+            var products = driver.FindElements(By.ClassName("product"));
+            //MessageBox.Show("Amount of products: "+ products.Count);
+
+            IWebElement sticker;
+            int i = 0;
+            foreach (var prd in products)
+            {
+                
+                try
+                {
+                    sticker = prd.FindElement(By.ClassName("sticker"));
+                    i++;
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine(exception);
+                    throw;
+                }
+                
+                
+            }
+            products[0].Click();
+            MessageBox.Show("Amount of stickers: " + i);
+
+
+            driver.Close();
+            driver.Quit();
+            driver = null;
         }
     }
 }

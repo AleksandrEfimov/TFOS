9447985d339fad50143e67d6497bdc5037cd33a0
AleksandrEfimov <aleks.efy@gmail.com>
2017-09-19 17:55:29 +0300

    Дописана проверка расположения стран в таблице
    Проверка проходит и по странам и по подзонам

--- TFOS/Form1.cs
+++ TFOS/Form1.cs
@@ -23,7 +23,7 @@ namespace TFOS
         }
 
         // логин в админ панель
-        void login(ref IWebDriver driver, ref WebDriverWait wait)
+        void logInAdmin(ref IWebDriver driver, ref WebDriverWait wait)
         {
             // заходим на страничку
             driver.Url = "http://localhost:8080/litecart/admin/";
@@ -49,7 +49,7 @@ namespace TFOS
             string log = "";
             IWebDriver driver = new ChromeDriver();
             WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
-            login(ref driver, ref wait);
+            logInAdmin(ref driver, ref wait);
             // как критерий входа в админ-панель - виджет статистики интернет-магазина
             try
             {
@@ -96,9 +96,11 @@ namespace TFOS
                             continue;
                         }
                     }
+                if (++i < countMenuItem-1 )
+                {
+                    menuItem[++i].Click();
+                }
 
-                menuItem[++i].Click();
-               
                 log += "Меню: " + i + ",\n";
 
             } while (i < countMenuItem);
@@ -150,93 +152,139 @@ namespace TFOS
         }
 
 
-
+        // TODO: сделать функцию перебора и сравнения строк в таблице
+        // MADE: сделаны функции обновления контекста: таблица и строки
         private void button3_Click(object sender, EventArgs e)
         {
+
             IWebDriver driver = new ChromeDriver();
             WebDriverWait wait = new WebDriverWait( driver, TimeSpan.FromSeconds(10));
 
             StringBuilder log = new StringBuilder("");
             IWebElement dataTable;
             IList<IWebElement> trows, zone, zoneNext;
-            
+            bool isEnteredzone = false;
 
-            void reffunc()
+            // функция обновления контекста - описание
+            void newContext()
             {
                 // таблица со странами
                 dataTable = driver.FindElement(By.ClassName("dataTable"));
                 // массив строк
                 trows = dataTable.FindElements(By.TagName("tr"));
             }
-
-
-            // вход в админку
-            login(ref driver, ref wait);
-            driver.Url = "http://localhost:8080/litecart/admin/?app=countries&doc=countries";
-
-            reffunc();
-            
-            bool isEnteredzone = false;
-
-            // TODO: сделать функцию перебора и сравнения строк в таблице
-            // void reSorting()
-            // 
-            //
-            for (int i = 1; i < trows.Count-2; i++)
+            // функция проверки сортивки стран и подзон по алфавиту - описание
+            void reSorting(ref IList<IWebElement> tRowSort)
             {
-                if (isEnteredzone)
+                for (int i = 1; i < tRowSort.Count - 2; i++)
                 {
-                    reffunc();
-                }
+                    if (isEnteredzone)
+                    {
+                        newContext();
+                    }
 
-                //// РЕФАКТОРИНГ!!!
-                //  Строки таблицы 
-                zone = trows[i].FindElements(By.TagName("td"));
-                zoneNext = trows[i+1].FindElements(By.TagName("td"));
-                // значения ячеек - названия стран
+
+                    //  Строки таблицы 
+                    zone = trows[i].FindElements(By.TagName("td"));
+                    zoneNext = trows[i + 1].FindElements(By.TagName("td"));
+                    // значения ячеек - названия стран
                     string str1 = zone[4].Text;
                     string str2 = zoneNext[4].Text;
-                // проверяем, что str2 в алфавитном порядке идёт после str1
+                    // проверяем, что str2 в алфавитном порядке идёт после str1
                     if (str2.CompareTo(str1) != 1)
-                    MessageBox.Show("Проверяемая строка-"+i+". Ошибка сортировки : "+str1+" и "+str2);
-                // логгируем
-                //  . . .
+                        MessageBox.Show("Проверяемая строка-" + i + ". Ошибка сортировки : " + str1 + " и " + str2);
+                    else
+                    {
+                        log.AppendLine(i + "-й шаг, страны: " + str1 + " " + str2 + "; ");
+                    }
 
-                                                   // проверка наличия подзон
-                                                //if (zone[5].Text != "0")
-                                                //{
-                                                //    // количество подзон
-                                                //    int AmountOfState = Convert.ToInt16(zone[5].Text);
 
-                                                //    if (AmountOfState > 0)
-                                                //    {
-                                                //        // входи в страну (не война)
-                                                //        zone[4].Click();
-                                                //        //// РЕФАКТОРИНГ!!!
-                                                //        // ищем таблицу
-                                                //        dataTable = driver.FindElement(By.ClassName("dataTable"));
-                                                //        // массив строк
-                                                //        trows = dataTable.FindElements(By.TagName("tr"));
+                    // для отображения прохождения по всем строкам
+                    zone[0].Click();
 
-                                                //        for (int j = 1; j < trows.Count; j++)
-                                                //        {
-                                                //             //Строки таблицы 
-                                                //IList<IWebElement> zone = trows[i].FindElements(By.TagName("td"));
-                                                //    IList<IWebElement> zoneNext = trows[i+1].FindElements(By.TagName("td"));
 
-                                                //        }
-                                                //        isEnteredzone = true;
-                                                //    }
-                                                //}
+                    // проверка наличия подзон, вход, сравнение
+                    if (zone[5].Text != "0")
+                    {
+                        // количество подзон
+                        int AmountOfState = Convert.ToInt16(zone[5].Text);
 
+                        if (AmountOfState > 0)
+                        {
+
+                            // входим в страну (не война)
+                            try
+                            {
+                                var refCountry = zone[4].FindElement(By.TagName("a"));
+                                MessageBox.Show("Переход к " + zone[4].Text);
+                                // переход к подзонам
+                                refCountry.Click();
+                                // обновление таблицы
+                                newContext();
+                            }
+                            catch (Exception ex)
+                            {
+                                MessageBox.Show("Проблема с переходом к подзонам" + ex.ToString());
+                            }
+
+
+                            // сравнение подзон
+                            for (int j = 1; j < trows.Count - 2; j++)
+                            {
+                                //Строки таблицы 
+                                zone = trows[j].FindElements(By.TagName("td"));
+                                zoneNext = trows[j + 1].FindElements(By.TagName("td"));
+                                // значения ячеек - названия стран
+                                str1 = zone[2].Text;
+                                str2 = zoneNext[2].Text;
+                                // проверяем, что str2 в алфавитном порядке идёт после str1
+                                if (str2.CompareTo(str1) != 1)
+                                    MessageBox.Show("Проверяемая строка-" + j + "  " + str1 + " и " + str2);
+                                else
+                                {
+                                    log.AppendLine(i + "." + j + " шаг, территории: " + str1 + " " + str2 + "; ");
+                                }
+                            }
+
+                            // меняем флаг захода в подменю
+                            isEnteredzone = true;
+                            var menuItem = driver.FindElements(By.Id("app-"));
+                            menuItem[2].Click();
+                        }
+
+                    }
+                    // вернулись в country - треба обновить строки.
+                    newContext();
+
+                }
 
             }
-        MessageBox.Show("Парам-парам-пам, ВСЁ!");
+            
+            // войти в админку
+            logInAdmin(ref driver, ref wait);
+
+            // перейти на адрес
+            driver.Url = "http://localhost:8080/litecart/admin/?app=countries&doc=countries";
+            // обновить контекст
+            newContext();
+            
+
+            try
+            {
+                reSorting(ref trows);
+                MessageBox.Show("Парам-парам-пам, ВСЁ!");
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.ToString());
+            }
 
             driver.Close();
             driver.Quit();
-
         }
+
+
+
 
         private void button4_Click(object sender, EventArgs e)
         {

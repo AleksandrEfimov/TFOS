eb9a6e311f41ffac0a2826e97e54bdc3a2a251b7
AleksandrEfimov <efimov@ap-team.ru>
2018-03-16 10:53:50 +0300

    Added class Product.Properties

---
+++ TFOS/Products.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TFOS
+{
+    class Products
+    {
+
+        public struct Properties
+        {
+            string Name { get; set; }
+            
+            struct RegularPrice
+            {
+                private float Price { get; set; }
+                private int[] ColorRGB;
+                private string FontSize;
+                private bool striked;
+
+            };
+
+            struct CampainPrice
+            {
+                private float Price { get; set; }
+                private int[] ColorRGB;
+                private string FontSize;
+            }
+            
+
+
+        }
+
+
+        }
+    }

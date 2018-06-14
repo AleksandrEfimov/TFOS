4930b113c4a8a160efe11e4bbaaeaf1bdd421098
Aleks.Efy <aleks.efy@gmail.com>
2018-06-14 10:33:00 +0300

    start user actions task
    start User Actions task

---
+++ TFOS/UserAction.cs
@@ -0,0 +1,46 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TFOS
+{
+    class UserAction
+    {
+        WebBrowserClient webBrCl = new WebBrowserClient();
+        IWebDriver driver;
+        public UserAction(string seturl)
+        {
+            driver = webBrCl.driver;
+            driver.Url = seturl;
+        }
+
+        public string SignUp()
+        {
+            driver.Url= "http://localhost:8080/litecart/en/create_account";
+            try
+            {
+                var SignUpForm = driver.FindElement(By.Name("customer_form"));
+                var tr = SignUpForm.FindElements(By.TagName("tr"));
+                var firstname = SignUpForm.FindElement(By.Name("firstname"));
+                //lastname, address1, postcode, city
+                // ddl 
+                string i = tr.Count.ToString();
+                return i;
+            }
+            catch (Exception ex)
+            {
+                return ex.ToString();
+            }
+        }
+
+
+
+
+
+
+
+    }
+}
